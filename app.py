a0b5505a2eb0d0fa2be127f2efd7432e61b08bc8
sebastian.estrada <sebastian.estrada@upr.edu>
2022-04-19 14:13:07 -0400

    Added User and admin classes with backend  and DB functionality

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 # -- Import section --
+from backend.user import User
+from crypt import methods
+from flask_pymongo import PyMongo
 from flask import (
     Flask,
     render_template,
@@ -7,7 +10,8 @@ from flask import (
     url_for,
     session
 )
-from flask_pymongo import PyMongo
+
+import model
 import gunicorn # for heroku deployment
 import secrets
 import bcrypt 
@@ -29,6 +33,9 @@ app.config['MONGO_URI'] = f"mongodb+srv://admin:{mongodb_password}@cluster0.3pto
 # Initialize PyMongo
 mongo = PyMongo(app, tlsCAFile=certifi.where())
 
+# Session Data/Cookie
+app.secret_key = secrets.token_urlsafe(16)
+
 # Collection References
 users = mongo.db.users
 posts = mongo.db.posts
@@ -42,3 +49,77 @@ INDEX route, initial route
 @app.route('/home')
 def index():
     return render_template('index.html')
+
+
+@app.route("/signup",methods=["GET","POST"])
+def signup():
+    if request.method == "POST":
+        # dictionary of possible errors to happen
+        error_message = {"message":"", "error":None}
+
+        # new user to be made
+        new_user = User.from_document({
+            "email":request.form["email"],
+            "username":request.form["username"],
+            "password": request.form["password"]
+        })
+
+        model.add_user(new_user,users,error_message)
+        if error_message["error"]:
+            return render_template("session.html",session=session,error_message=error_message["message"])
+        session["username"] = new_user.username
+        return render_template("session.html",session=session,error_message=error_message["message"])
+
+    else:
+        if session.get("username"):
+            return redirect(url_for("index"))
+        return render_template("session.html")
+
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        # dictionary of possible errors to happen
+        error_message = {"message":"", "error":None}
+
+        user_authentication =  User.from_document({
+            "email": "TO_LOGIN",
+            "username":request.form["username"],
+            "password":request.form["password"]
+        })
+
+        model.authenticate_user(user_authentication,users,message)
+        if error_message["error"]:
+            return render_template("session.html",session=session,error_message=error_message["error"])
+
+        session["username"] = user_authentication.username
+        return redirect(url_for("index"))
+    else:
+        if session.get("username"):
+            return redirect(url_for("index"))
+        return render_template("login.html",session=session)
+        
+
+"""
+Method that allows the user to logout their account from the page's current session
+Returns:
+    redirects user to main page (index.html) with their account logged out
+"""
+@app.route("/logout")
+def logout():
+    # clear user from session
+    session.clear()
+    return redirect(url_for("index"))
+
+
+
+
+
+
+
+
+        
+        
+
+
+    
---
+++ backend/tests/user_test.py
@@ -0,0 +1,106 @@
+from backend import user
+import unittest
+
+
+# Data For Initial Tests
+test_email = "email@email.com"
+test_username = "sael@17"
+test_password = "colegio@2019"
+
+class TestUser(unittest.TestCase):
+
+     # User instances to test
+    user01 = user.User(test_email,test_username,test_password)
+    user02 = user.User("yan.aquino1@upr.edu","yanc@aquino","123456789")
+    user03 = user.User("brandong.fung@techexchange.in","branfung","bran.fung12")
+    user04 = user.User("user04@email.com","user04","user01_is_the_best")
+
+
+    def test_argument_types(self):
+
+        # Everything has to be a string
+        self.assertRaises(TypeError,user.User,123,test_username,test_password)
+        self.assertRaises(TypeError,user.User,["Harry","Potter"],test_username,test_password)
+        self.assertRaises(TypeError,user.User,("Star","Wars"),test_username,test_password)
+        self.assertRaises(TypeError,user.User,{"Hello":"There"},test_username,test_password)
+        self.assertRaises(TypeError,user.User,True,test_username,test_password)
+        self.assertRaises(TypeError,user.User,False,test_username,test_password)
+        self.assertRaises(TypeError,user.User,None,test_username,test_password)
+
+        self.assertRaises(TypeError,user.User,test_email,12345678910,test_password)
+        self.assertRaises(TypeError,user.User,test_email,[True,False],test_password)
+        self.assertRaises(TypeError,user.User,test_email,("Bilbo","Baggins"),test_password)
+        self.assertRaises(TypeError,user.User,test_email,{"Luke":"Skywalker"},test_password)
+        self.assertRaises(TypeError,user.User,test_email,True,test_password)
+        self.assertRaises(TypeError,user.User,test_email,False,test_password)
+        self.assertRaises(TypeError,user.User,test_email,None,test_password)
+
+        self.assertRaises(TypeError,user.User,test_email,test_username,123423556336)
+        self.assertRaises(TypeError,user.User,test_email,test_username,[1,2])
+        self.assertRaises(TypeError,user.User,test_email,test_username,("Darth","Vader"))
+        self.assertRaises(TypeError,user.User,test_email,test_username,({"Emperor":"Palpatine"}))
+        self.assertRaises(TypeError,user.User,test_email,test_username,True)
+        self.assertRaises(TypeError,user.User,test_email,test_username,False)
+        self.assertRaises(TypeError,user.User,test_email,test_username,None)
+
+
+    def test_argument_values(self):
+        self.assertRaises(ValueError,user.User,"user",test_username,test_password)
+        self.assertRaises(ValueError,user.User,"thebest@in@",test_username,test_password)
+        self.assertRaises(ValueError,user.User,"thebest@in@email",test_username,test_password)
+        self.assertRaises(ValueError,user.User,"thebest@in@email.com",test_username,test_password)
+        self.assertRaises(ValueError,user.User," ",test_username,test_password)
+
+
+        self.assertRaises(ValueError,user.User,test_email,"user",test_password)
+        self.assertRaises(ValueError,user.User,test_email,"user01_is_the-best-user-in-PR",test_password)
+        self.assertRaises(ValueError,user.User,test_email," ",test_password)
+        self.assertRaises(ValueError,user.User,test_email,"",test_password)
+        
+        self.assertRaises(ValueError,user.User,test_email,test_username,"12345")
+        self.assertRaises(ValueError,user.User,test_email,test_username,"this is the best password in the world jajaajaj")
+        self.assertRaises(ValueError,user.User,test_email,test_username," ")
+        self.assertRaises(ValueError,user.User,test_email,test_username, "")
+
+    def test_pw_to_star(self):
+        self.assertEqual(len(self.user01.password),len(self.user01.password))
+        self.assertEqual(len(self.user02.password),len(self.user02.password))
+        self.assertEqual(len(self.user03.password),len(self.user03.password))
+        self.assertEqual(len(self.user04.password),len(self.user04.password))
+
+
+    def test_can_moderate(self):
+        self.assertEqual(self.user01.canModerate,False)
+        self.assertEqual(self.user02.canModerate,False)
+        self.assertEqual(self.user03.canModerate,False)
+        self.assertEqual(self.user04.canModerate,False)
+
+    def test_to_document(self):
+        self.assertEqual(self.user01.to_document(),{"email":test_email,"username":test_username,
+        "password":test_password})
+        self.assertEqual(self.user02.to_document(),{"email":"yan.aquino1@upr.edu",
+        "username":"yanc@aquino","password":"123456789"})
+        self.assertEqual(self.user03.to_document(),{"email":"brandong.fung@techexchange.in",
+        "username":"branfung","password":"bran.fung12"})
+        self.assertEqual(self.user04.to_document(),{"email":"user04@email.com","username":"user04",
+        "password":"user01_is_the_best"})
+
+
+    def test_from_document(self):
+        # user01 = user.User(test_email,test_username,test_password)
+        # user02 = user.User("yan.aquino1@upr.edu","yanc@aquino","123456789")
+        # user03 = user.User("brandong.fung@techexchange.in","branfung","bran.fung12")
+        # user04 = user.User("user04@email.com","user04","user01_is_the_best")
+        temp_user = user.User.from_document({"email":"juan.delcampo@upr.edu",
+        "username":"juan.delcampo@1","password":"google@2022"})
+
+        # test object has been created
+        self.assertEqual(temp_user.canModerate,False)
+        # test if to document works too
+        self.assertEqual(temp_user.to_document,{"email":"juan.delcampo@upr.edu",
+        "username":"juan.delcampo@1","password":"google@2022"})
+
+if __name__ == '__main__':
+    unittest.main()
+
+
