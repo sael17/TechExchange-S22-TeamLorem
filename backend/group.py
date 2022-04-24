466f2c75e2ae931701935a539909010f4502aba0
branfung <branfung@hotmail.com>
2022-04-24 18:48:56 -0400

    group/post input validation

--- backend/group.py
+++ backend/group.py
@@ -5,14 +5,13 @@ class Group():
     
     def __init__(self, creator: ObjectId, name: str, about: str, date_created):
         self.creator = creator
-        self.name = name
-        self._camel_case_name = self.name_to_camel(self.name)
+        self.name = self.strip_spaces(name)
         self.about = about
         self.date_created = date_created
         
         # Sets of ObjectIds
-        self.users = []
-        self.posts = []
+        # self.users = []
+        # self.posts = []
 
     
     @classmethod
@@ -20,10 +19,7 @@ class Group():
         """Constructs and returns a new Group from a JSON Document """
         
         group = Group(document['creator'], document['name'],document['about'], document['date_created'])
-       
-        # group.users = document['users']
-        # group.posts = document['posts']
-        
+               
         return group
 
     def to_document(self):
@@ -33,19 +29,9 @@ class Group():
             "creator":self.creator,
             "name": self.name,
             "about": self.about,
-            "users":self.users,
-            "posts":self.posts,
             "date_created":self.date_created
         }
 
-    def name_to_camel(self, name):
-        
-        # regex to represent ANY character that isn't a letter
-        words = split('([^a-zA-Z])', name)
-        
-        camel = []
-        for word in words:
-            if word.isalpha():
-                camel.append(word.capitalize())
-                
-        return ''.join(camel)        
+    def strip_spaces(self, name):
+        words = split(' ', name)
+        return ''.join(words)        
